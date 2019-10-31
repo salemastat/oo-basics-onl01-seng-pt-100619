6f54baec13ec54ba7724aa530ee1bc33c6692916
Salem Asrat <salemasrat@gmail.com>
2019-10-31 17:50:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :properties, :color, :size, :material, :condition, :cobble
+  attr_accessor :color, :size, :material, :condition, :cobble
   attr_reader :brand
  
   def initialize(brand)
